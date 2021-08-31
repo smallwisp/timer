c062a40d3ee60e219177f9a13aec7c98663d3c04
smallwisp <4ubarov96@mail.ru>
2021-08-31 14:41:25 +0300

    completed tasks of lesson 18

--- script/script.js
+++ script/script.js
@@ -72,19 +72,13 @@ window.addEventListener('DOMContentLoaded', () => {
       function handlerMenuJS({timing, draw, duration}) {
          let start = performance.now();
 
-         /* if (screenWidth >= 768) {
-            
-         } */
-
          requestAnimationFrame(function animate(time) {
-         // timeFraction изменяется от 0 до 1
          let timeFraction = (time - start) / duration;
          if (timeFraction > 1) timeFraction = 1;
 
-         // вычисление текущего состояния анимации
          let progress = timing(timeFraction);
 
-         draw(progress); // отрисовать её
+         draw(progress); 
 
          if (timeFraction < 1) {
             requestAnimationFrame(animate);
